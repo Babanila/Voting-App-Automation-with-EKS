2263d29d891529a5d7dd47b90d2ce763f7a0cd4f
Babajide Williams <babanila@yahoo.com>
2026-05-09 11:53:12 +0200

    feat: Add backend S3 bootstrap package

---
+++ infra/terraform-bootstrap/variables.tf
@@ -0,0 +1,23 @@
+variable "project_bucket_name" {
+  type = string
+}
+
+variable "key_pair_name" {
+  description = "The name of the AWS key pair to use for SSH access"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
+variable "author_name" {
+  type        = string
+  description = "Used for naming resources"
+
+  validation {
+    condition     = can(regex("^[a-z0-9-]+$", lower(var.author_name)))
+    error_message = "author_name must contain only lowercase letters, numbers, and hyphens."
+  }
+}
